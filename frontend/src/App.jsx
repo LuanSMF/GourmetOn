72c4b936d8e7c58dbbff1ae9f05608741c1ae6e8
Luan Sá Muniz <luansamunizsf@gmail.com>
2026-09-21 23:51:57 -0300

    feat:adicionado receitas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,12 @@ function App() {
   useEffect(() => {
     async function carregarReceitas() {
       try{
-        const url = import.meta.env.DEV
-          ? "http://localhost:3001/api/receitas?query=pasta"
-          : "/api/receitas?query=pasta";
-        const resposta= await fetch(url)
-        
+        //const url = import.meta.env.DEV
+        //  ? "http://localhost:3001/api/receitas?query=pasta"
+        //  : "/api/receitas?query=pasta";
+        //const resposta= await fetch(url)
+        const resposta = await fetch("/receitas.json")
+
         if(!resposta.ok){
           throw new Error("Não foi possível carregar os produtos.");
         }
@@ -55,13 +56,45 @@ function App() {
 
       </header>
         <main>
+
+        </main>
+        <section>
+
+        </section>
+        <section>
             <div>
               <button onClick={()=> setCategoriaSelecionada("Todos")}>Todos</button>
               <button onClick={()=> setCategoriaSelecionada("Massas")}>Massas</button>
               <button onClick={()=> setCategoriaSelecionada("Saudaveis")}>Saudáveis</button>
               <button onClick={()=> setCategoriaSelecionada("Sobremesas")}>Sobremesas</button>
             </div>
-        </main>
+            <section>
+              {carregando &&(
+                <div className='loading'>
+                  <div className='spinner'></div>
+                  <p>Carregando Receitas...</p>
+                </div>
+              )}
+              {erro && <p>{erro}</p>}
+
+              {!carregando && !erro && receitasFiltradas.map((receitas) =>(
+              <article key={receitas.id}>
+                <h3>{receitas.title}</h3>
+                <img src={receitas.image}></img>
+                <a href=''>↗</a>
+              </article>  
+              ))}
+            </section>
+        </section>
+        <section>
+
+        </section>
+        <footer>
+          <h4>Uma Oferta gostosa na sua caixa de entrada.</h4>
+          <span>Cadastre seu e-mail para receber novidades e promoções</span>
+          <input placeholder='Seu melhor e-mail' id='email'></input>
+          <button onClick={()=> salvarDados()}>Cadastrar ➡</button>
+        </footer>
     </>
   )
 }
